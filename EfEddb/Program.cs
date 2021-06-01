476f9a8938fd6361fdd0b8030e73e368a0cde46b
Greg Doud <gpdoud@gmail.com>
2021-06-01 10:38:03 -0400

    Add project files.

---
+++ EfEddb/Program.cs
@@ -0,0 +1,36 @@
+﻿using EfEddb.Models;
+
+using System;
+using System.Linq;
+
+namespace EfEddb {
+    class Program {
+        static void Main(string[] args) {
+
+            var context = new EdDbContext();
+
+            var studentsWithMajors = from s in context.Students
+                                     join m in context.Majors
+                                     on s.MajorId equals m.Id
+                                     where s.StateCode == "KY"
+                                     orderby s.Lastname
+                                     select new { 
+                                         s.Firstname, s.Lastname, Major = m.Description 
+                                     };
+            foreach(var s in studentsWithMajors) {
+                Console.WriteLine($"{s.Firstname} {s.Lastname} || {s.Major}");
+            }
+
+            #region KY students by lastname
+            //var studentsInKy = context.Students
+            //                            .Where(s => s.StateCode == "KY")
+            //                            .OrderBy(s => s.Lastname)
+            //                            .ToList();
+            //foreach(var s in studentsInKy) {
+            //    Console.WriteLine($"{s.Firstname} {s.Lastname}");
+            //}
+            #endregion
+
+        }
+    }
+}
